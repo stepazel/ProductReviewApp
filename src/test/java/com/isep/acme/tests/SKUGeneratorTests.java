ec4f0e59e9cc0e703f4793f7f9695f9d3c4ba83e
Hugo <1201669@isep.ipp.pt>
2023-11-05 15:14:17 +0000

    Update SKUGeneratorTests.java (#25)

--- src/test/java/com/isep/acme/tests/SKUGeneratorTests.java
+++ src/test/java/com/isep/acme/tests/SKUGeneratorTests.java
@@ -1,21 +1,26 @@
 package com.isep.acme.tests;
 
+import com.isep.acme.services.SKUGeneratorHashBasedImpl;
 import com.isep.acme.services.SKUGeneratorRandomPatternImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-public class SKUGeneratorTests {
+class SKUGeneratorTests {
     private final SKUGeneratorRandomPatternImpl skuGeneratorFirst;
+    private final SKUGeneratorHashBasedImpl     skuGeneratorSecond;
 
     SKUGeneratorTests() {
-        skuGeneratorFirst = new SKUGeneratorRandomPatternImpl();
+        skuGeneratorFirst  = new SKUGeneratorRandomPatternImpl();
+        skuGeneratorSecond = new SKUGeneratorHashBasedImpl();
     }
 
     @Test
     void firstGeneratorTest() {
-        var sku = skuGeneratorFirst.generateNew("test - this parameter is not needed");
-        assert sku.length() == 11;
+        var sku = skuGeneratorFirst.generateNew("");
+        assert sku.length() == 12;
 
         assert Character.isLetter(sku.charAt(1));
         assert Character.isLetter(sku.charAt(3));
@@ -34,6 +39,27 @@ public class SKUGeneratorTests {
         assert !Character.isLetter(sku.charAt(10)) && !Character.isDigit(sku.charAt(10));
     }
 
+    @Test
+    void secondGeneratorTest() {
+        String sku = skuGeneratorSecond.generateNew("Table");
+
+        // check if the length is 12
+        assertEquals(12, sku.length());
+
+        // compare with the expected value
+        assertEquals("1a880773abed", sku);
+
+        // check if the same product name generates the same SKU
+        assertEquals(sku, skuGeneratorSecond.generateNew("Table"));
+
+        String sku2 = skuGeneratorSecond.generateNew("Wardrobe");
+
+        // check if the length is 12
+        assertEquals(12, sku2.length());
+
+        // compare with the expected value
+        assertEquals("6343f7bdcec1", sku2);
+    }
 
 
 }
