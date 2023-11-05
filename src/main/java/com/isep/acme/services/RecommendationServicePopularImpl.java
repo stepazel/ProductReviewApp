3599d06bea4764e4e0ef6e12119d5c1cf9e6c924
Hugo Miranda (1201669) <1201669@isep.ipp.pt>
2023-11-05 00:53:40 +0000

    Renamed FirstRecommendationServiceImpl -> RecommendationServicePopularImpl

--- src/main/java/com/isep/acme/services/RecommendationServicePopularImpl.java
+++ src/main/java/com/isep/acme/services/RecommendationServicePopularImpl.java
@@ -8,16 +8,16 @@ import com.isep.acme.repositories.ReviewRepository;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FirstRecommendationServiceImpl implements RecommendationService {
+public class RecommendationServicePopularImpl implements RecommendationService {
     private final ReviewRepository reviewRepository;
 
-    public FirstRecommendationServiceImpl(ReviewRepository reviewRepository) {
+    public RecommendationServicePopularImpl(ReviewRepository reviewRepository) {
         this.reviewRepository = reviewRepository;
     }
 
     @Override
     public List<ReviewDTO> getReviewRecommendedForUser(Long userId) {
-        var reviews = reviewRepository.findAll();
+        var reviews            = reviewRepository.findAll();
         var recommendedReviews = new ArrayList<Review>();
         for (Review review : reviews) {
             if (review.getCountOfVotes() >= 4 && (double) review.getUpVote().size() / review.getCountOfVotes() > 0.65) {
